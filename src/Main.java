82c83b9e9f06890dea70aae11ea877e3bf0f0ba6
Fursa <fursad14@mail.ru>
2022-09-25 18:03:12 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+int account = 200;
+int transfer = 5000;
 
-
+int bonus = transfer / 100;
+if (transfer > 1000){
+    bonus = transfer / 100;
+} else {
+    bonus = 0;
+}
+        System.out.println("Счет: " + (account + transfer + bonus) + " рублей");
+        System.out.println("Сумма пополнения: " + transfer  + " рублей");
+        System.out.println("Бонусные рубли: " + bonus + " рублей");
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
+        //
         // суммы пополнения и тп
 
         // Условным оператором проверяете превысила ли
